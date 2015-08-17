605062a510f433397f24e5f1efd691e1590366e5
Philip Read <Philip.Read.000@gmail.com>
2015-08-17 09:53:55 -0500

    Moved KeyCode constants to own file.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # jConsole
-Javascript application that simulates a unix console without text field. Inttended to be used as a base engine for console based games.
+JavaScript application that simulates a Unix console without text field. Intended to be used as a base engine for console based games.
--- consoleScript.js
+++ consoleScript.js
@@ -45,17 +45,17 @@ $(document).keydown(function(event) {
 	}
 	var keyCode = event.keyCode;
 	
-	if(keyCode == App.keyCode.LEFT_ARROW) {
+	if(keyCode == Keys.keyCode.LEFT_ARROW) {
 		if(cursorPosition > 0) {
 			setCursorVals(cursorPosition - 1, cursorMargin + 1 );
 		}
 	}
-	else if(keyCode == App.keyCode.RIGHT_ARROW) {
+	else if(keyCode == Keys.keyCode.RIGHT_ARROW) {
 		if(cursorPosition < $('#input').text().length) {
 			setCursorVals(cursorPosition + 1, cursorMargin - 1 );
 		}
 	}
-	else if(keyCode == App.keyCode.UP_ARROW) {
+	else if(keyCode == Keys.keyCode.UP_ARROW) {
 		if(
 		historyIndex > 0) {
 			var newInput = commandHistory[--historyIndex];
@@ -63,23 +63,23 @@ $(document).keydown(function(event) {
 			setCursorVals(newInput.length, 0);
 		}
 	}
-	else if(keyCode == App.keyCode.DOWN_ARROW) {
+	else if(keyCode == Keys.keyCode.DOWN_ARROW) {
 		if(historyIndex < commandHistory.length - 1) {
 			var newInput = commandHistory[++historyIndex];
 			$('#input').html(newInput);
 			setCursorVals(newInput.length, 0);
 		}
 	}
-	else if(keyCode == App.keyCode.HOME) { 
+	else if(keyCode == Keys.keyCode.HOME) { 
 		setCursorVals(0, $('#input').text().length);
 	}
-	else if(keyCode == App.keyCode.END) { 
+	else if(keyCode == Keys.keyCode.END) { 
 		setCursorVals($('#input').text().length, 0);
 	}
-	else if(keyCode == App.keyCode.CAPS_LOCK) {
+	else if(keyCode == Keys.keyCode.CAPS_LOCK) {
 		capsOn = !capsOn;
 	}
-	else if(keyCode >= App.keyCode.KEY_A && keyCode <= App.keyCode.KEY_Z) {
+	else if(keyCode >= Keys.keyCode.KEY_A && keyCode <= Keys.keyCode.KEY_Z) {
 		var newChar;
 		if(event.ctrlKey) {
 			// So the user can copy
@@ -104,10 +104,10 @@ $(document).keydown(function(event) {
 		}
 		appendNewChar(newChar);
 	}
-	else if(keyCode == App.keyCode.SPACE) {
+	else if(keyCode == Keys.keyCode.SPACE) {
 		appendNewChar('&nbsp');
 	}
-	else if(keyCode == App.keyCode.BACKSPACE) { 
+	else if(keyCode == Keys.keyCode.BACKSPACE) { 
 		//Deletes highlighted text, else if not highlighted backspaces once
 		if(deleteSelected() == false) { 
 			if(cursorPosition > 0) {
@@ -115,10 +115,10 @@ $(document).keydown(function(event) {
 			}
 		}
 	}
-	else if(keyCode == App.keyCode.DELETE) {
+	else if(keyCode == Keys.keyCode.DELETE) {
 		deleteSelected();
 	}
-	else if(keyCode >= App.keyCode.KEY_0 && keyCode <= App.keyCode.KEY_9) { 
+	else if(keyCode >= Keys.keyCode.KEY_0 && keyCode <= Keys.keyCode.KEY_9) { 
 		var newChar;
 		if(event.shiftKey) {
 			newChar = shiftNumChar(keyCode);
@@ -128,11 +128,11 @@ $(document).keydown(function(event) {
 		}
 		appendNewChar(newChar);
 	}
-	else if(keyCode >= App.keyCode.NUMPAD_0 && keyCode <= App.keyCode.NUMPAD_9) { 
+	else if(keyCode >= Keys.keyCode.NUMPAD_0 && keyCode <= Keys.keyCode.NUMPAD_9) { 
 		var newChar = String.fromCharCode(keyCode - 48);
 		appendNewChar(newChar);
 	}
-	else if(keyCode == App.keyCode.ENTER) {
+	else if(keyCode == Keys.keyCode.ENTER) {
 		commandExecuting = true;
 		var curInput = $('#input').text();
 		addToHistory(curInput);
@@ -181,34 +181,34 @@ function cursorBlink() {
 function shiftNumChar(keyCode) {
 	var charCode;
 	switch(keyCode) {
-		case App.keyCode.KEY_0: // )
+		case Keys.keyCode.KEY_0: // )
 			charCode = 41; 
 			break;
-		case App.keyCode.KEY_1: // !
+		case Keys.keyCode.KEY_1: // !
 			charCode = 33;
 			break;
-		case App.keyCode.KEY_2: // @
+		case Keys.keyCode.KEY_2: // @
 			charCode = 64;
 			break;
-		case App.keyCode.KEY_3: // #
+		case Keys.keyCode.KEY_3: // #
 			charCode = 35;
 			break;
-		case App.keyCode.KEY_4: // $
+		case Keys.keyCode.KEY_4: // $
 			charCode = 36;
 			break;
-		case App.keyCode.KEY_5: // %
+		case Keys.keyCode.KEY_5: // %
 			charCode = 37;
 			break;
-		case App.keyCode.KEY_6: // ^
+		case Keys.keyCode.KEY_6: // ^
 			charCode = 94;
 			break;
-		case App.keyCode.KEY_7: // &
+		case Keys.keyCode.KEY_7: // &
 			charCode = 38;
 			break;
-		case App.keyCode.KEY_8: // *
+		case Keys.keyCode.KEY_8: // *
 			charCode = 42;
 			break;
-		case App.keyCode.KEY_9: // (
+		case Keys.keyCode.KEY_9: // (
 			charCode = 40;
 			break;
 	}
@@ -290,107 +290,3 @@ function scrollPrint() {
 		commandExecuting = false;
 	}
 }
-
-var App = {}
-
-App.keyCode = {
-	BACKSPACE: 8,
-	TAB: 9,
-	ENTER: 13,
-	SHIFT: 16,
-	CTRL: 17,
-	ALT: 18,
-	PAUSE: 19,
-	CAPS_LOCK: 20,
-	ESCAPE: 27,
-	SPACE: 32,
-	PAGE_UP: 33,
-	PAGE_DOWN: 34,
-	END: 35,
-	HOME: 36,
-	LEFT_ARROW: 37,
-	UP_ARROW: 38,
-	RIGHT_ARROW: 39,
-	DOWN_ARROW: 40,
-	INSERT: 45,
-	DELETE: 46,
-	KEY_0: 48,
-	KEY_1: 49,
-	KEY_2: 50,
-	KEY_3: 51,
-	KEY_4: 52,
-	KEY_5: 53,
-	KEY_6: 54,
-	KEY_7: 55,
-	KEY_8: 56,
-	KEY_9: 57,
-	KEY_A: 65,
-	KEY_B: 66,
-	KEY_C: 67,
-	KEY_D: 68,
-	KEY_E: 69,
-	KEY_F: 70,
-	KEY_G: 71,
-	KEY_H: 72,
-	KEY_I: 73,
-	KEY_J: 74,
-	KEY_K: 75,
-	KEY_L: 76,
-	KEY_M: 77,
-	KEY_N: 78,
-	KEY_O: 79,
-	KEY_P: 80,
-	KEY_Q: 81,
-	KEY_R: 82,
-	KEY_S: 83,
-	KEY_T: 84,
-	KEY_U: 85,
-	KEY_V: 86,
-	KEY_W: 87,
-	KEY_X: 88,
-	KEY_Y: 89,
-	KEY_Z: 90,
-	LEFT_META: 91,
-	RIGHT_META: 92,
-	SELECT: 93,
-	NUMPAD_0: 96,
-	NUMPAD_1: 97,
-	NUMPAD_2: 98,
-	NUMPAD_3: 99,
-	NUMPAD_4: 100,
-	NUMPAD_5: 101,
-	NUMPAD_6: 102,
-	NUMPAD_7: 103,
-	NUMPAD_8: 104,
-	NUMPAD_9: 105,
-	MULTIPLY: 106,
-	ADD: 107,
-	SUBTRACT: 109,
-	DECIMAL: 110,
-	DIVIDE: 111,
-	F1: 112,
-	F2: 113,
-	F3: 114,
-	F4: 115,
-	F5: 116,
-	F6: 117,
-	F7: 118,
-	F8: 119,
-	F9: 120,
-	F10: 121,
-	F11: 122,
-	F12: 123,
-	NUM_LOCK: 144,
-	SCROLL_LOCK: 145,
-	SEMICOLON: 186,
-	EQUALS: 187,
-	COMMA: 188,
-	DASH: 189,
-	PERIOD: 190,
-	FORWARD_SLASH: 191,
-	GRAVE_ACCENT: 192,
-	OPEN_BRACKET: 219,
-	BACK_SLASH: 220,
-	CLOSE_BRACKET: 221,
-	SINGLE_QUOTE: 222
-};
